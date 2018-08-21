ec5127cb74ad554fc70b4715c407e36d6d17f945
hhnguyen <hhnguyen@sfu.ca>
2018-08-21 13:32:25 -0700

    added mysql interface, created db init script

---
+++ fridge_tracker/db/db_init.js
@@ -0,0 +1,53 @@
+var mysql = require('mysql');
+var fs = require('fs');
+
+var str = fs.readFileSync('../../data/login.txt', 'ascii')
+str = str.replace('\r', '')
+var arr = str.split('\n')
+
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: arr[0].toString(),
+    password: arr[1].toString()
+});
+
+conn.connect(function(err) { 
+    if (err) throw err;
+    console.log("+++ Connected to MySQL DB on nhanngx");
+});
+
+conn.query("CREATE DATABASE fridge_tracker", function (err, result) {
+    if (err) throw err;
+    console.log("++ Database created");
+});
+
+conn.query("USE fridge_tracker");
+
+conn.query("CREATE TABLE users( \
+            id int NOT NULL AUTO_INCREMENT PRIMARY KEY, \
+            name text NOT NULL )", function (err, result) {
+                                        if (err) throw err;
+                                        console.log("+ Table 'users' created");
+});
+
+conn.query("CREATE TABLE food_item( \
+            id int NOT NULL AUTO_INCREMENT PRIMARY KEY, \
+            name text, \
+            category text, \
+            bought date, \
+            location int, \
+            quantity float, \
+            unit text, \
+            expiry date, \
+            owner int NOT NULL, \
+            FOREIGN KEY (owner) \
+                REFERENCES users(id) \
+                ON UPDATE CASCADE )", function (err, result) {
+                                            if (err) throw err;
+                                            console.log("+ Table 'food_item' created");
+});
+
+conn.end(function(err) {
+    if (err) throw err;
+    console.log("+++ Init successful, exiting.");
+});
---
+++ fridge_tracker/db/draft.js
@@ -0,0 +1,5 @@
+var fs = require('fs');
+
+var f = fs.readFileSync('../../data/login.txt', 'ascii')
+
+console.log(f)
